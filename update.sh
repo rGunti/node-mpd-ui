0e84980b164552f9f291a471e9b2438be0658bc7
Raphael Guntersweiler <raphael@rgunti.ch>
2017-06-24 19:30:20 +0200

    Added a line that automatically makes bash scripts executable

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+SCRIPT_PATH=`realpath $0`
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+
+cd $SCRIPT_DIR
+echo $SCRIPT_DIR
 
 echo "Please wait while we are getting the latest version from GitHub..."
 
 git reset --hard HEAD
 git clean -xffd
 git pull
+
+echo "Making Bash Scripts executable..."
+chmod +x *.sh
